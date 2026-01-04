e3aa00ecde30fbdd2b4962db5cbaa65557eb0342
WesleyRomain <146979701+WesleyRomain@users.noreply.github.com>
2026-01-04 16:43:07 +0100

    Users CRUD afgewerkt

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//Test
 router.get('/', (req,res) =>{
     res.json({ message: "Posts route werkt!"})
 })
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,127 @@
-const express= require ('express');
+const express = require('express');
 const router = express.Router();
+const db = require('../db');
 
-router.get('/', (req, res) =>{
-    res.json({ message: "Users route werkt"})
-})
+//Verkrijgen (GET) van alle users
+
+router.get('/', async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM users'); //Probeer de user-records op te halen in de database.
+        res.json(rows); //
+    } catch (err) { // Lukt het niet, toon dan een error.
+        res.status(500).json({ erro: err.message });
+    }
+
+});
+
+//Verkrijgen (GET) voor één entiteit
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    //Haal de ID uit de URL, bv. /api/users/5 -> id =5
+
+    try {
+        const sql = 'SELECT * FROM users WHERE id= ?';
+        const [rows] = await db.query(sql, [id]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ error: "User niet gevonden" })
+        }
+
+        res.json(rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+
+
+});
+
+//Toevoegen (POST) van een nieuwe user
+router.post('/', async (req, res) => {
+    const { first_name, last_name, email } = req.body;
+
+    //Basisvalidatie van gegevens
+    if (!first_name || !last_name || !email) {
+        return res.status(400).json({ error: "Alle velden zijn verplicht" }); //Bad Request foutmelding (fout ligt bij gebruiker)
+    }
+
+    try {
+        const sql = 'INSERT INTO users (first_name, last_name, email) VALUES (?, ?, ?)';
+        const [result] = await db.query(sql, [first_name, last_name, email])
+
+        res.status(201).json({ // Iets correct toegevoegd
+            message: "User toegevoegd",
+            user_id: result.insertId
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+});
+
+//Updaten van een bestaande user (PUT)
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    // Haal de ID uit de URL, bv. /api/users/5 -> id =5
+
+    const { first_name, last_name, email } = req.body
+    // Haal de nieuwe waarden uit de body van de request
+
+    // Basisvalidatie: velden mogen niet leeg zijn
+    if (!first_name || !last_name || !email) {
+        return res.status(400).json({ error: "Alle velden zijn verplicht" })
+    }
+
+    try {
+        // Eerst controleren of user bestaat
+        const [existing] = await db.query('SELECT * FROM users WHERE id= ?', [id]);
+
+        if (existing.length === 0) {
+            return res.status(404).json({ error: "User niet gevonden " });
+            //404 = Not Found -> user bestaat niet
+        }
+
+        //SQL-query om user te updaten
+        const sql = `
+        UPDATE users 
+        SET first_name= ?, last_name= ?, email = ?
+        WHERE id= ?`
+
+        await db.query(sql, [first_name, last_name, email, id]);
+
+        res.json({
+            message: "User succesvol geüpdatet",
+            update_id: id
+        });
+
+    } catch (err) {
+        // 500: Internal Server error -> fout in de server of database
+        res.status(500).json({ error: err.message });
+    }
+});
+
+//Verwijderen (DELETE) van een user
+router.delete('/:id', async(req, res) => {
+    const {id} = req.params;
+    // Haal de ID uit de URL, bv. /api/users/5 -> id = 5
+
+    try {
+        //Eerst controleren of user bestaat
+        const [existing] = await db.query('SELECT * FROM users WHERE id= ?', [id]);
+
+        if(existing.length === 0){
+            return res.status(404).json({ error: "User niet gevonden"});
+            // 404 = Not Found -> user bestaat niet
+        }
+
+        await db.query('DELETE FROM users WHERE id= ?', [id]);
+
+        res.json({
+            message: "User succesvol verwijderd",
+            delete_id: id
+        });
+    } catch (err) {
+        //500 = Internal Server Error -> Fout in server of database
+        res.status(500).json({ error: err.message})
+    }
+});
 
 module.exports = router;
